3da3bf6a9d93f70a722142097afd07bc12aab0f1
Bryan Leboff <leboff@gmail.com>
2013-02-07 15:43:42 -0500

    created filter

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,6 +3,12 @@
 /* Controllers */
 
 
-function HomeController() {}
-HomeController.$inject = [];
+function HomeController($scope) {
+    //http://flickr.com/services/auth/?api_key=[api_key]&perms=[perms]&api_sig=[api_sig]
+    $scope.auth = {
+        api_key: '2d7a4c8d1bd92eecc2b914e433d7e2c1',
+        perms: 'read'
+    }
+}
+HomeController.$inject = ['$scope'];
 
--- app/js/filters.js
+++ app/js/filters.js
@@ -7,4 +7,17 @@ angular.module('myApp.filters', []).
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     }
-  }]);
+  }])
+  .filter('flicker_sign', function(){
+        var secret = '93e768161447831f';
+        return function(params){
+            var sig = secret;
+            var url = 'http://flickr.com/services/auth/?'
+            _.each(params, function(value, key){
+                sig+=key+value;
+                url+='&'+key+'='+value;
+            });
+            url += 'api_sig='+sig
+            return url;
+        }
+    });
