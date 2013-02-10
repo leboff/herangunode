beb57421b0b265295cbd1ddb06e27d7e950e3649
Bryan Leboff <leboff@gmail.com>
2013-02-10 17:37:35 -0500

    grab images

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,7 @@
 /* Controllers */
 
 
-function HomeController($scope, InstagramToken) {
+function HomeController($scope, $http, InstagramToken, instagram_search_url) {
     //get instagram link
     $scope.token = InstagramToken();
 
@@ -17,24 +17,44 @@ function HomeController($scope, InstagramToken) {
         $scope.positionError = ("Error: " + errors[error.code]);
     };
     var positionAcquired = function(position){
-        $scope.postition = position;
         console.log('position acquired', position);
-        $scope.$apply();
+        searchImages(position);
     };
-    if ($scope.token && navigator.geolocation) {
-        var timeoutVal = 10 * 1000 * 1000;
-        navigator.geolocation.getCurrentPosition(
-            positionAcquired,
-            geoError,
-            { enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
-        );
-    }
-    else {
-        alert("Geolocation is not supported by this browser");
+    var geolocate = function(){
+        if ($scope.token && navigator.geolocation) {
+            var timeoutVal = 10 * 1000 * 1000;
+            navigator.geolocation.getCurrentPosition(
+                positionAcquired,
+                geoError,
+                { enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
+            );
+        }
+        else {
+            alert("Geolocation is not supported by this browser");
+        }
+    };
+    geolocate();
+
+    //grab the images and process them
+    var successCallback = function(resp, status, headers, config){
+        $scope.images = resp.data;
+    };
+    var searchImages = function(position){
+        //https://api.instagram.com/v1/media/search?lat=48.858844&lng=2.294351&?access_token=249962630.f59def8.4c1defa23e9c4c8e969c299d84029f8e
+        var config = {
+            params: {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+                access_token: InstagramToken(),
+                callback: 'JSON_CALLBACK'
+            }
+        };
+        $http.jsonp(instagram_search_url, config).success(successCallback);
     }
 
+
 }
-HomeController.$inject = ['$scope', 'InstagramToken'];
+HomeController.$inject = ['$scope', '$http', 'InstagramToken', 'instagram_search_url'];
 
 function InstagramAuthController($location, InstagramToken){
     var hash = $location.hash();
--- app/js/services.js
+++ app/js/services.js
@@ -7,16 +7,17 @@
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
   value('version', '0.1').
-    factory('InstagramToken', ['$cookies', function($cookies) {
-        var token;
-        return function(tkn){
-            if(tkn){
-                token = $cookies.token = tkn;
-            }else{
-                if(!token && $cookies.token){
-                    token = $cookies.token;
-                }
+  value('instagram_search_url', 'https://api.instagram.com/v1/media/search').
+  factory('InstagramToken', ['$cookies', function($cookies) {
+    var token;
+    return function(tkn){
+        if(tkn){
+            token = $cookies.token = tkn;
+        }else{
+            if(!token && $cookies.token){
+                token = $cookies.token;
             }
-            return token;
         }
-    }]);
+        return token;
+    }
+  }]);
