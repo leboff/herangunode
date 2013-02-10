1d98ab2de0b5b5d47d56b9673ffd34a4e3a88926
Bryan Leboff <leboff@gmail.com>
2013-02-10 17:03:55 -0500

    geo positioned

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,9 +5,34 @@
 
 function HomeController($scope, InstagramToken) {
     //get instagram link
-    if(InstagramToken()){
-        console.log(InstagramToken())
+    $scope.token = InstagramToken();
+
+    //geolocation stuff!
+    var geoError = function(error){
+        var errors = {
+            1: 'Permission denied',
+            2: 'Position unavailable',
+            3: 'Request timeout'
+        };
+        $scope.positionError = ("Error: " + errors[error.code]);
+    };
+    var positionAcquired = function(position){
+        $scope.postition = position;
+        console.log('position acquired', position);
+        $scope.$apply();
+    };
+    if ($scope.token && navigator.geolocation) {
+        var timeoutVal = 10 * 1000 * 1000;
+        navigator.geolocation.getCurrentPosition(
+            positionAcquired,
+            geoError,
+            { enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
+        );
     }
+    else {
+        alert("Geolocation is not supported by this browser");
+    }
+
 }
 HomeController.$inject = ['$scope', 'InstagramToken'];
 
